02d55c3af512f165d2d5a8f1e6ea6eb6bb4f87c0
Kevin Charm <kevin@kevincharm.com>
2024-10-28 00:23:26 +0100

    add base deployment

--- scripts/deployBase.ts
+++ scripts/deployBase.ts
@@ -1,15 +1,20 @@
 import { ethers, ignition, run } from 'hardhat'
 import { WeightedRaffleFactory__factory } from '../typechain-types'
-import WeightedRaffleImplModule from '../ignition/modules/WeightedRaffleImpl'
+import WeightedRaffleImpl from '../ignition/modules/WeightedRaffleImpl'
 import WeightedRaffleFactoryImpl from '../ignition/modules/WeightedRaffleFactory'
+import assert from 'node:assert'
+
+const BASE_ANYRAND_ADDRESS = '0xF6baf607AC2971EE6A3C47981E7176134628e36C'
+const BASE_WEIGHTED_RAFFLE_ADMIN_MULTISIG = '0x0c77A8F2970f27967abDC0Db557fc8fC90B4F615'
 
 async function main() {
+    const [deployer] = await ethers.getSigners()
     const chainId = await ethers.provider.getNetwork().then((network) => network.chainId)
 
-    const { weightedRaffleImpl } = await ignition.deploy(WeightedRaffleImplModule)
+    const { weightedRaffleImpl } = await ignition.deploy(WeightedRaffleImpl)
     const factoryInitData = WeightedRaffleFactory__factory.createInterface().encodeFunctionData(
         'init',
-        [await weightedRaffleImpl.getAddress()],
+        [await weightedRaffleImpl.getAddress(), BASE_ANYRAND_ADDRESS],
     )
     const { weightedRaffleFactoryProxy } = await ignition.deploy(WeightedRaffleFactoryImpl, {
         parameters: {
@@ -20,6 +25,17 @@ async function main() {
     })
     console.log(
         `WeightedRaffleFactory deployed at: ${await weightedRaffleFactoryProxy.getAddress()}`,
+    )
+
+    // Transfer ownership to the admin multisig
+    const factory = await WeightedRaffleFactory__factory.connect(
+        await weightedRaffleFactoryProxy.getAddress(),
+        deployer,
+    )
+    await factory.transferOwnership(BASE_WEIGHTED_RAFFLE_ADMIN_MULTISIG).then((tx) => tx.wait(1))
+    assert(
+        (await factory.owner()) === BASE_WEIGHTED_RAFFLE_ADMIN_MULTISIG,
+        'Ownership transfer failed',
     )
 
     // Verify all
